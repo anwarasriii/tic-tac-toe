89a2b255ba14cd7463c5c8b9f09a910a2962b463
anwarasriii <muhdanwar209@gmail.com>
2026-06-01 20:43:11 +0800

    update line format

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,32 @@ const App = () => {
   const [turn, setTurn] = useState('X');
   const [winner, SetWinner] = useState('None');
 
-  const findWinner = (board,turn) => {
+  const findWinner = (board, turn) => {
 
     const findDiagonal = [];
     const findOtherDiagonal = [];
 
-    for(let i = 0; i < size; i++){
+    for (let i = 0; i < size; i++) {
       const index = i * size + i;
       findDiagonal.push(board[index] === turn)
     }
-    if(findDiagonal.every(Boolean)){
+    if (findDiagonal.every(Boolean)) {
       return true
     }
 
-    for(let i = 0; i < size; i++){
+    for (let i = 0; i < size; i++) {
       const index = i * size + (size - 1 - i);
       findOtherDiagonal.push(board[index] === turn)
     }
-    if(findOtherDiagonal.every(Boolean)){
+    if (findOtherDiagonal.every(Boolean)) {
       return true
     }
 
-    for(let i = 0; i< size; i++){
+    for (let i = 0; i < size; i++) {
       const findRow = [];
       const findColumn = [];
 
-      for(let j = 0; j < size; j++){
+      for (let j = 0; j < size; j++) {
         const indexRow = i * size + j;
         findRow.push(board[indexRow] === turn);
 
@@ -43,7 +43,7 @@ const App = () => {
         findColumn.push(board[indexColumn] === turn);
 
       }
-      if(findColumn.every(Boolean) || findRow.every(Boolean)){
+      if (findColumn.every(Boolean) || findRow.every(Boolean)) {
         return true;
       }
     }
@@ -52,15 +52,15 @@ const App = () => {
   }
 
   const handleClick = (index) => {
-    if (board[index] || winner !== 'None') return;   
+    if (board[index] || winner !== 'None') return;
 
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard)
 
-    const isWinner = findWinner(newBoard,turn);
+    const isWinner = findWinner(newBoard, turn);
 
-    if(isWinner){
+    if (isWinner) {
       SetWinner(turn)
     } else {
       setTurn(turn === 'X' ? 'O' : 'X');
@@ -70,15 +70,15 @@ const App = () => {
   return (
     <div className='container'>
       <h2>Winner: {winner}</h2>
-    <div className='board'>
-      {
-      board.map((value, index) => (
-  <button key={index} onClick={() => handleClick(index)}>
-    {value}
-  </button>
-))
-    }
-    </div>
+      <div className='board'>
+        {
+          board.map((value, index) => (
+            <button key={index} onClick={() => handleClick(index)}>
+              {value}
+            </button>
+          ))
+        }
+      </div>
     </div>
   )
 }
